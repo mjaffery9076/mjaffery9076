2a2da0fa429949fd6596dd15dc03ecd1124cf7bb
mjaffery9076 <115514573+mjaffery9076@users.noreply.github.com>
2022-10-10 21:28:58 -0400

    Add files via upload

---
+++ ATM.pysource.py
@@ -0,0 +1,46 @@
+
+#opening display on program initialization
+print("Welcome to Gomez Bank!")
+print("Please insert your card.")
+#sleep function, more realistic user experience
+
+print('Please enter your pin: ')
+Pin = input("Enter your PIN: ")
+
+#sleep function, more realistic user experience
+#next step is to call user pin input from another python program, chapter 7
+
+
+#call from data storage, display user name
+with open("Datalist ATM.py",'a') as f:
+    with open('Datalist ATM.py') as file:
+        data = list(file)
+
+print('Welcome back')
+#insert user name after welcome back
+
+print("Press 1 to view Checking account balance,")
+print("Press 2 to view Savings account balance,")
+print("Press 3 to withdraw,")
+print("Press 4 to deposit.")
+
+
+def var1(name):
+#This function calls from the data stored, still need to figure out how to diffrentiate the terms in the list.
+#Create a function to display the existing balance
+
+def var2(name):
+#This function calls from the data stored, still need to figure out how to diffrentiate the terms in the list.
+#Create a function to display the existing balance
+
+def var3(name):
+#This function calls from the data stored, still need to figure out how to diffrentiate the terms in the list.
+#Create a function to subtract from the existing balance
+
+def var4(name):
+#This function calls from the data stored, still need to figure out how to diffrentiate the terms in the list.
+#Create a function to add to the existing balance
+        
+
+
+
